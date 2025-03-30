020e855abac5c4467200c2cc1282c031ed4797a1
sidhart25 <navneetnishchal1420@gmail.com>
2025-03-31 04:32:24 +0530

    solved cheapest flight using limited bfs type approach

---
+++ leetcodeProblems/dp/cheapestFlightWithKStops/solution.go
@@ -0,0 +1,48 @@
+package cheapestflightwithkstops
+
+// simple approach
+// time complexity --> O(k*n)
+func solutionOne(n int, flights [][]int, src int, dst int, k int) int {
+	cost := make([]int, n)
+	for i := 0; i < n; i++ {
+		cost[i] = -1
+	}
+
+	cost[src] = 0
+
+	for stop := 0; stop <= k; stop++ {
+		temp := make([]int, n)
+
+		copy(temp, cost)
+
+		for _, flight := range flights {
+			from := flight[0]
+			to := flight[1]
+			p := flight[2]
+
+			if cost[from] == -1 {
+				continue
+			}
+
+			total := cost[from] + p
+
+			if temp[to] == -1 || temp[to] > total {
+				temp[to] = total
+			}
+
+			if temp[to] == -1 || temp[to] > total {
+				temp[to] = total
+			}
+		}
+		cost = temp
+
+	}
+
+	return cost[dst]
+
+}
+
+func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
+	return solutionOne(n, flights, src, dst, k)
+
+}
